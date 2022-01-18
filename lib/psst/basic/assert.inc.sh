61cf9078c4c353127565fc623601f2331d33c9a8
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2022-01-18 15:53:38 +0100

    Add argument check assertions

--- lib/psst/basic/assert.inc.sh
+++ lib/psst/basic/assert.inc.sh
@@ -19,13 +19,178 @@ INCLUDE_SEEN_PSST="$INCLUDE_SEEN_PSST _assert.inc.sh_"
 #	for signals and lower values are used by functions as failure indicators.
 #
 # PARAMETERS
-# 	msg: Message to print to stderr
+# 	msg: Message to print to stderr.
 #
 # SAMPLE
-# 	test [ "$index" -gt 0 ] || assert_fail_psst "Index must be > 0"
+# 	[ "$index" -gt 0 ] || assert_fail_psst "Index must be > 0"
 #
 assert_fail_psst()
 {
-	printf "%s\n" "$1"
+	assert_argc_psst "assert_fail_psst" 1 $#
+	assert_hasarg_psst "assert_argc_psst" "msg" "$1"
+
+	printf "Assertion fail: %s!\n" "$1" >&2
 	exit 127
+}
+
+
+##
+# FUNCTION
+# 	assert_argc_psst <func> <expected> <actual>
+#
+# SUMMARY
+# 	Prints an error to stderr if `actual` is not equal `expected`` and
+#	terminates current process with error 127, which is the highest possible
+#	error as values 128 and up are reserved for signals and lower values are
+#	used by functions as failure indicators.
+#
+# PARAMETERS
+# 	func: Name of the function.
+#	expected: Number of aguments expected.
+#	actual: Number of actual aguments.
+#
+# SAMPLE
+#	assert_argc_psst "myfunc" 3 $#
+#
+assert_argc_psst()
+{
+	if [ $# -ne 3 ]
+	then
+		printf "Assertion fail: Function %s expects %s arguments, got %s!" \
+			assert_argc_psst 3 $# >&2
+		exit 127
+	fi
+
+	assert_hasarg_psst "assert_argc_psst" "func" "$1"
+	assert_hasarg_psst "assert_argc_psst" "expected" "$2"
+	assert_hasarg_psst "assert_argc_psst" "actual" "$3"
+
+	if [ "$2" -ne "$3" ]
+	then
+		printf "Assertion fail: Function %s expects %s arguments, got %s!" \
+			"$@" >&2
+		exit 127
+	fi
+}
+
+
+##
+# FUNCTION
+# 	assert_minargc_psst <func> <min> <actual>
+#
+# SUMMARY
+#	Prints an error to stderr if `actual` is smaller than `min` and terminates
+#	current process with error 127, which is the highest possible error as
+#	values 128 and up are reserved for signals and lower values are used by
+#   functions as failure indicators.
+
+# PARAMETERS
+# 	func: Name of the function.
+#	min: Minimum number of aguments expected.
+#	actual: Number of actual aguments.
+#
+# SAMPLE
+#	assert_minargc_psst "myfunc" 3 $#
+#
+assert_minargc_psst()
+{
+	assert_argc_psst "assert_minargc_psst" 3 $#
+	assert_hasarg_psst "assert_minargc_psst" "func" "$1"
+	assert_hasarg_psst "assert_minargc_psst" "min" "$2"
+	assert_hasarg_psst "assert_minargc_psst" "actual" "$3"
+
+	if [ "$2" -gt "$3" ]
+	then
+		printf "%s: Function %s expects at least %s arguments, got %s!" \
+			"Assertion fail" "$@" >&2
+		exit 127
+	fi
+}
+
+
+##
+# FUNCTION
+# 	assert_maxargc_psst <func> <max> <actual>
+#
+# SUMMARY
+#	Prints an error to stderr if `actual` is bigger than `max` and terminates
+#	current process with error 127, which is the highest possible error as
+#	values 128 and up are reserved for signals and lower values are used by
+#   functions as failure indicators.
+
+# PARAMETERS
+# 	func: Name of the function.
+#	max: Maximum number of aguments expected.
+#	actual: Number of actual aguments.
+#
+# SAMPLE
+#	assert_maxargc_psst "myfunc" 3 $#
+#
+assert_maxargc_psst()
+{
+	assert_argc_psst "assert_minargc_psst" 3 $#
+	assert_hasarg_psst "assert_minargc_psst" "func" "$1"
+	assert_hasarg_psst "assert_minargc_psst" "max" "$2"
+	assert_hasarg_psst "assert_minargc_psst" "actual" "$3"
+
+	if [ "$2" -lt "$3" ]
+	then
+		printf "%s: Function %s expects at most %s arguments, got %s!" \
+			"Assertion fail" "$@" >&2
+		exit 127
+	fi
+}
+
+
+##
+# FUNCTION
+# 	assert_hasarg_psst <func> <arg> [<value>]
+#
+# SUMMARY
+#	Prints an error to stderr if `value` is empty and terminates current
+#	process with error 127, which is the highest possible error as values 128
+#	and up are reserved for signals and lower values are used by functions as
+#	failure indicators. If `value` is not given, expands `arg` instead.
+#
+# PARAMETERS
+# 	func: Name of the function.
+#	arg: Name of the argument.
+#
+# SAMPLE
+#	assert_hasarg_psst "myfunc" "myarg"
+#	assert_hasarg_psst "myfunc" "myarg" "$1"
+#
+assert_hasarg_psst()
+{
+	if [ $# -lt 2 ]
+	then
+		printf "%s: Function %s expects at least %s arguments, got %s!" \
+			"Assertion fail" assert_argc_psst 2 $# >&2
+		exit 127
+	fi
+
+	if [ $# -gt 3 ]
+	then
+		printf "%s: Function %s expects at most %s arguments, got %s!" \
+			"Assertion fail" assert_argc_psst 3 $# >&2
+		exit 127
+	fi
+
+	if [ $# = 3 ]
+	then
+		if [ -z "$3" ]
+		then
+			printf "%s: Argument %s of function %s must not be empty!" \
+				"Assertion fail" "$2" "$1" >&2
+			exit 127
+		fi
+		return
+	fi
+
+	if [ -z "$(eval "echo \$$2")" ]
+	then
+		printf "%s: Argument %s of function %s must not be empty!" \
+			"Assertion fail" "$2" "$1" >&2
+		exit 127
+	fi
 }
---
+++ test/bin/test_assert.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env sh
+
+# Exit at once if a command fails and the error isn't caught
+set -e
+
+cmdBase=$( dirname "$0" )
+
+if [ -z "$INCLUDE_PSST" ]; then
+	INCLUDE_PSST="$cmdBase/../../lib/psst"
+fi
+
+# shellcheck source=../../lib/psst/basic.inc.sh
+. "$INCLUDE_PSST/basic.inc.sh"
+
+# =============================================================================
+
+# Assert fail must always fail
+set +e
+( assert_fail_psst "test" 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+set -e
+
+# assert_argc must only fail if argument number is wrong
+set +e
+( assert_argc_psst "test" 3 2 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+
+( assert_argc_psst "test" 2 3 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+
+( assert_argc_psst "test" 3 3 2>/dev/null ) || test_fail_psst $LINENO
+set -e
+
+# assert_minargc must only fail if argument number is too low
+set +e
+( assert_minargc_psst "test" 3 2 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+
+( assert_minargc_psst "test" 2 3 2>/dev/null ) || test_fail_psst $LINENO
+
+( assert_minargc_psst "test" 3 3 2>/dev/null ) || test_fail_psst $LINENO
+set -e
+
+# assert_maxargc must only fail if argument number is too high
+set +e
+( assert_maxargc_psst "test" 3 2 2>/dev/null ) || test_fail_psst $LINENO
+
+( assert_maxargc_psst "test" 2 3 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+
+( assert_maxargc_psst "test" 3 3 2>/dev/null ) || test_fail_psst $LINENO
+set -e
+
+#  assert_hasarg must fail only if arg is empty
+set +e
+( assert_hasarg_psst "test" "arg" "x" 2>/dev/null ) || test_fail_psst $LINENO
+
+( assert_hasarg_psst "test" "arg" "" 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+
+( arg="x" ; assert_hasarg_psst "test" "arg" 2>/dev/null ) \
+	|| test_fail_psst $LINENO
+
+# shellcheck disable=SC2034 # arg is used indirectly by name
+( arg="" ; assert_hasarg_psst "test" "arg" 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+set -e
