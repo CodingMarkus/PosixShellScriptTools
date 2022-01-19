cf40c15cd20f7cd3ae9f404d53a22105768ec772
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2022-01-20 00:35:58 +0100

    Add globbing tests

--- test/bin/test_glob.sh
+++ test/bin/test_glob.sh
@@ -15,8 +15,114 @@ fi
 
 # =============================================================================
 
-#echo "$( glob_max_psst 5 "../data/glob/*.txt" )"
+# Glob must accept at least one argument
+set +e
+( glob_psst 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+set -e
 
-#echo "$( glob_max_psst 7 "../data/glob/* .space" )"
+# Argument must not be empty
+set +e
+( glob_psst "" 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+set -e
 
-#echo "$( glob_r_psst "../data/glob/*.txt" )"
+
+# Glob max must accept at least two arguments
+set +e
+( glob_max_psst 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+
+( glob_max_psst 1 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+set -e
+
+# Arguments must not be empty
+set +e
+( glob_max_psst "" 1 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+
+( glob_max_psst 1 "" 2>/dev/null )
+[ $? = 127 ] || test_fail_psst $LINENO
+set -e
+
+# Test globbing is correct
+(
+	tmpDir="/tmp"
+	tempdir_psst "tmpDir" || test_fail_psst $LINENO
+
+	globDst="$tmpDir/glob_psst"
+
+	glob_psst "../data/glob/testdir/*" \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob.txt" \
+		|| test_fail_psst $LINENO
+
+
+	glob_psst "../data/glob/testdir/*.txt" \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob_txt.txt" \
+		|| test_fail_psst $LINENO
+
+
+	glob_psst "../data/glob/testdir/*.space" \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob_space.txt" \
+		|| test_fail_psst $LINENO
+
+
+	glob_psst "../data/glob/testdir/*.space" \
+		"../data/glob/testdir/*.txt"         \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob_txt_space.txt" \
+		|| test_fail_psst $LINENO
+)
+
+
+# Test globbing max is correct
+(
+	tmpDir="/tmp"
+	tempdir_psst "tmpDir" || test_fail_psst $LINENO
+
+	globDst="$tmpDir/glob_psst"
+
+	glob_max_psst 3 "../data/glob/testdir/*" \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob_3.txt" \
+		|| test_fail_psst $LINENO
+
+
+	glob_max_psst 3 "../data/glob/testdir/*.txt" \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob_txt_3.txt" \
+		|| test_fail_psst $LINENO
+
+
+	glob_max_psst 3 "../data/glob/testdir/*.space" \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob_space_3.txt" \
+		|| test_fail_psst $LINENO
+
+
+	glob_max_psst 3 "../data/glob/testdir/*.space" \
+		"../data/glob/testdir/*.txt"               \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob_space_3.txt" \
+		|| test_fail_psst $LINENO
+
+
+	glob_max_psst 3 "../data/glob/testdir/*.txt" \
+		"../data/glob/testdir/*.space"           \
+		| tr "$FS_CHAR_PSST" "$NL_CHAR_PSST" >"$globDst"
+
+	cmp -s "$globDst" "$cmdBase/../data/glob/glob_txt_3.txt" \
+		|| test_fail_psst $LINENO
+)
